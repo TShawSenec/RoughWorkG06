bece9d513443a78d9f399bad80bcbeb1658c13ce
TShawSenec <50424828+TShawSenec@users.noreply.github.com>
2020-07-19 15:51:44 -0400

    Add files via upload

---
+++ StartStop/StartStopWorkoutActivity.java
@@ -0,0 +1,13 @@
+package com.example.fitnessapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StartStopWorkoutActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startstop);
+    }
+}
